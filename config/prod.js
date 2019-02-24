b39aca22144d6407aea0879b76815ee0ce2608ea
RadPete <pete.2392@hotmail.com>
2019-02-24 16:17:44 -0600

    work on auth flow

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+// prod.js - production keys here
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
+
+//  Client ID: '698246150078-gffdkl9qd6nk00fllgpus7rijt50gmuv.apps.googleusercontent.com'
+// Client Secret: 9zIdVCApVBy1DXvhpf3Z8lpq
+// mongo URI :  mongodb+srv://pete6148:tOl5jaNjxgIYF9pS@emaily-prod-u5tai.mongodb.net/test?retryWrites=true
+// cookieKey:  xeNc05L9Ir
