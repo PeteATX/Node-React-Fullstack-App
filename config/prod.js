55e11927cd972a949699328b2574d91a3961e198
RadPete <pete.2392@hotmail.com>
2019-02-25 18:12:30 -0600

    Changes for Proxy Google Strategy

--- config/prod.js
+++ config/prod.js
@@ -6,7 +6,3 @@ module.exports = {
 	cookieKey: process.env.COOKIE_KEY
 };
 
-//  Client ID: '698246150078-gffdkl9qd6nk00fllgpus7rijt50gmuv.apps.googleusercontent.com'
-// Client Secret: 9zIdVCApVBy1DXvhpf3Z8lpq
-// mongo URI :  mongodb+srv://pete6148:tOl5jaNjxgIYF9pS@emaily-prod-u5tai.mongodb.net/test?retryWrites=true
-// cookieKey:  xeNc05L9Ir
